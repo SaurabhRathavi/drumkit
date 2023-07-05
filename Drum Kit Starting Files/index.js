721b9d695c14be016087b5245b2964b1ae297331
Saurabh Rathavi <sdrathavi1112@gmail.com>
2023-07-05 20:17:58 +0530

    last

--- Drum Kit Starting Files/index.js
+++
@@ -1,7 +0,0 @@
-let numberOfDrumButtons = document.querySelector(".drum").length;
-
-for (let i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("button was clicked");
-  });
-}
